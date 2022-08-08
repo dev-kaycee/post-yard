9f74b37196c3a1ff2da3532cfa1f4c39dcfd9619
Happy Nkanyane <hthabang23@gmail.com>
2022-08-08 17:52:09 +0200

    login view, login url

--- postYard/user_auth/urls.py
+++ postYard/user_auth/urls.py
@@ -2,11 +2,11 @@ from atexit import register
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='perform_register'),
     path('register', RegisterView.as_view(), name='register'),
-    # path('login', LoginView.as_view(), name='login'),
-    # path('login', LoginView.as_view(), name='perform_login'),
+    path('login', LoginView.as_view(), name='login'),
+    path('login', LoginView.as_view(), name='perform_login'),
 ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- postYard/user_auth/views.py
+++ postYard/user_auth/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views import View
-# from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.contrib import messages
 
 from .forms import NewUserRegistrationForm
 
-# Create your views here.
+
 class RegisterView(View):
     def get(self, request):
         form = NewUserRegistrationForm()
@@ -24,3 +24,23 @@ class RegisterView(View):
         messages.error(request, "Unsuccessfull. invalid details")
         # form = NewUserRegistrationForm()
         return render(request=request, template_name="register.html", context={"register_form":form})
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # TODO user validation
+        user_obj = authenticate(request, username=username, password=password) 
+
+        if user_obj is not None:
+            login(request, user_obj)
+            user = request.user.username
+            print(user)
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return render(request, 'login.html')
