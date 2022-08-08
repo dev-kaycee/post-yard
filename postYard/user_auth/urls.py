c44c3f176a80edd66ba9d91aafc59a45c9c4afda
Happy Nkanyane <hthabang23@gmail.com>
2022-08-08 17:44:09 +0200

    done! register

--- postYard/user_auth/urls.py
+++ postYard/user_auth/urls.py
@@ -1,11 +1,12 @@
+from atexit import register
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path
-from .views import LoginView, RegisterView, HomeView 
+from .views import RegisterView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='perform_register'),
-    path('register', RegisterView.as_view()),
+    path('register', RegisterView.as_view(), name='register'),
     # path('login', LoginView.as_view(), name='login'),
     # path('login', LoginView.as_view(), name='perform_login'),
 ] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
